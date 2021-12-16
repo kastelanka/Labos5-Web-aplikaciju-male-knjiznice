96d5c14b0ba0a788ac8b418eebe42ce60c8e883b
Ana Lovric <alovric@dspace.hr>
2021-12-16 19:08:32 +0100

    Add project files.

---
+++ Login.ascx
@@ -0,0 +1,18 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Login.ascx.cs" Inherits="labos5.Login" %>
+<asp:Panel ID="PanelLogin" runat="server">
+    <h1>Welcome!</h1>
+    <h2>Please enter your username and password to login</h2>
+    <asp:Label ID="Label1" runat="server" BorderStyle="None" Text="Username:"></asp:Label>
+    <asp:TextBox ID="TextBoxUsername" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" BorderStyle="None" Text="Password:"></asp:Label>
+    <asp:TextBox ID="TextBoxPassword" runat="server"></asp:TextBox>
+    <br />
+    <asp:Button ID="ButtonLogin" runat="server" Text="Login" OnClick="ButtonLogin_Click" />
+</asp:Panel>
+<asp:Panel ID="PanelError" runat="server" Visible="False">
+    <h3>Username or password not valid.</h3></asp:Panel>
+<asp:Panel ID="PanelDisplay" runat="server" Visible="False">
+    <asp:GridView ID="GridViewData" runat="server">
+    </asp:GridView>
+</asp:Panel>
---
+++ Login.ascx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace labos5
+{
+    public partial class Login : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //provjera korisnika, privatna metoda:
+        private bool CheckUserNamePassword()
+        {
+            //klasa za citanje XMLova:
+            XElement korisnici = XElement.Load(@"C:\Users\AnaLo\source\repos\labos5\App_Data\korisnici.xml");//relative pathove!!
+            var users = from user in korisnici.Elements("korisnik") 
+                select new
+                {
+                    username = (String)user.Element("korisnickoIme"),
+                    password = (String)user.Element("lozinka")
+                };
+
+            foreach (var user in users)
+            {
+                //if (user.username == TextBoxUsername.Text) && (user.password == TextBoxPassword.Text))
+                if((string.Compare(user.username, TextBoxUsername.Text, true) == 0)
+                && ((user.password == TextBoxPassword.Text)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void DisplayBooks()
+        {
+            PanelDisplay.Visible = true;
+            using (DataSet ds = new  DataSet())
+            {
+                ds.ReadXml(@"C:\Users\AnaLo\source\repos\labos5\App_Data\popisKnjiga.xml");
+                GridViewData.DataSource = ds;
+                GridViewData.DataBind();
+            }
+        }
+
+        protected void ButtonLogin_Click(object sender, EventArgs e)
+        {
+            //1. je li postoji taj username i password:
+            if (!CheckUserNamePassword())
+            {
+                PanelError.Visible = true;
+            }
+            else
+            {
+                DisplayBooks();
+
+            }
+        }
+    }
+}
